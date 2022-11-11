00dfd9ec3ea6ddb74910ddb65456abe52907f37f
cjwade <89665017+cjwade@users.noreply.github.com>
2022-11-10 22:00:34 -0800

    Added image selector

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { onSnapshot, collection, query, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
+import { HiOutlineSparkles } from "react-icons/hi";
+import Input from "./Input";
 
 const Feed = () => {
 	const [posts, setPosts] = useState([]);
@@ -16,7 +18,15 @@ const Feed = () => {
 
 	console.log(posts);
 	return (
-   <div className="">Feed</div>
+   <div className="sm:ml-[81px] xl:ml-[340px] w-[600px] min-h-screen border-r border-gray-400 text-white py-2">
+
+		<div className="stick top-0 bg-black flex justify-between font-medium text-[20px] px-4 py-2">
+			Home
+			<HiOutlineSparkles />
+		</div>
+
+		<Input />
+	</div>
    )
 };
 
